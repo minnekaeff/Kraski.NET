e88fb18a5829d53a46f7b1c6a4c610b715ae110f
minnekaeff <minnekaev1996@gmail.com>
2017-04-14 18:35:25 +0300

    /

---
+++ Kraski.NET/Kraski.NET.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Kraski.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pbColor = new System.Windows.Forms.PictureBox();
+            this.btnPencil = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnTriangle = new System.Windows.Forms.Button();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnEllipse = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnFractal = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudZoom = new System.Windows.Forms.NumericUpDown();
+            this.nudX = new System.Windows.Forms.NumericUpDown();
+            this.nudY = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbColor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudZoom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudY)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbColor
+            // 
+            this.pbColor.BackColor = System.Drawing.Color.Black;
+            this.pbColor.Location = new System.Drawing.Point(12, 12);
+            this.pbColor.Name = "pbColor";
+            this.pbColor.Size = new System.Drawing.Size(20, 20);
+            this.pbColor.TabIndex = 0;
+            this.pbColor.TabStop = false;
+            this.pbColor.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnPencil
+            // 
+            this.btnPencil.Location = new System.Drawing.Point(12, 67);
+            this.btnPencil.Name = "btnPencil";
+            this.btnPencil.Size = new System.Drawing.Size(95, 23);
+            this.btnPencil.TabIndex = 1;
+            this.btnPencil.Text = "Карандаш";
+            this.btnPencil.UseVisualStyleBackColor = true;
+            this.btnPencil.Click += new System.EventHandler(this.btnPencil_Click);
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(12, 96);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(95, 23);
+            this.btnLine.TabIndex = 2;
+            this.btnLine.Text = "Прямая";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // btnTriangle
+            // 
+            this.btnTriangle.Location = new System.Drawing.Point(12, 125);
+            this.btnTriangle.Name = "btnTriangle";
+            this.btnTriangle.Size = new System.Drawing.Size(95, 23);
+            this.btnTriangle.TabIndex = 3;
+            this.btnTriangle.Text = "Треугольник";
+            this.btnTriangle.UseVisualStyleBackColor = true;
+            this.btnTriangle.Click += new System.EventHandler(this.btnTriangle_Click);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Location = new System.Drawing.Point(12, 154);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(95, 23);
+            this.btnRectangle.TabIndex = 4;
+            this.btnRectangle.Text = "Прямоугольник";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Location = new System.Drawing.Point(12, 183);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(95, 23);
+            this.btnCircle.TabIndex = 5;
+            this.btnCircle.Text = "Окружность";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // btnEllipse
+            // 
+            this.btnEllipse.Location = new System.Drawing.Point(12, 212);
+            this.btnEllipse.Name = "btnEllipse";
+            this.btnEllipse.Size = new System.Drawing.Size(95, 23);
+            this.btnEllipse.TabIndex = 6;
+            this.btnEllipse.Text = "Эллипс";
+            this.btnEllipse.UseVisualStyleBackColor = true;
+            this.btnEllipse.Click += new System.EventHandler(this.btnEllipse_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(38, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(69, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(113, 11);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(780, 374);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 38);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(95, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnFractal
+            // 
+            this.btnFractal.Location = new System.Drawing.Point(12, 350);
+            this.btnFractal.Name = "btnFractal";
+            this.btnFractal.Size = new System.Drawing.Size(95, 35);
+            this.btnFractal.TabIndex = 10;
+            this.btnFractal.Text = "Построить";
+            this.btnFractal.UseVisualStyleBackColor = true;
+            this.btnFractal.Click += new System.EventHandler(this.btnFractal_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Фрактал Жулиа";
+            // 
+            // nudZoom
+            // 
+            this.nudZoom.Location = new System.Drawing.Point(64, 272);
+            this.nudZoom.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudZoom.Name = "nudZoom";
+            this.nudZoom.Size = new System.Drawing.Size(43, 20);
+            this.nudZoom.TabIndex = 12;
+            this.nudZoom.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudX
+            // 
+            this.nudX.Location = new System.Drawing.Point(64, 298);
+            this.nudX.Name = "nudX";
+            this.nudX.Size = new System.Drawing.Size(43, 20);
+            this.nudX.TabIndex = 13;
+            // 
+            // nudY
+            // 
+            this.nudY.Location = new System.Drawing.Point(64, 324);
+            this.nudY.Name = "nudY";
+            this.nudY.Size = new System.Drawing.Size(43, 20);
+            this.nudY.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Zoom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "По X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "По Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 396);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudY);
+            this.Controls.Add(this.nudX);
+            this.Controls.Add(this.nudZoom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFractal);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnEllipse);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.btnRectangle);
+            this.Controls.Add(this.btnTriangle);
+            this.Controls.Add(this.btnLine);
+            this.Controls.Add(this.btnPencil);
+            this.Controls.Add(this.pbColor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kraski.NET";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbColor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudZoom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudY)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pbColor;
+        private System.Windows.Forms.Button btnPencil;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnTriangle;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnEllipse;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnFractal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudZoom;
+        private System.Windows.Forms.NumericUpDown nudX;
+        private System.Windows.Forms.NumericUpDown nudY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Kraski.NET/Kraski.NET.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kraski.NET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool isPressed = false; // нажата ли кнопка мыши
+        Pen brush = new Pen(Color.Black, 1); // кисть
+        int tool = 1; // инструмент
+        float X1, Y1, X2, Y2; // текущее положение (X1, Y1) и конечное положение (X2, Y2)
+        Graphics G; // поверхность рисования
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            G = pictureBox2.CreateGraphics();
+        }
+
+        // параметры кисти
+        private void pictureBox1_Click(object sender, EventArgs e) // изменение цвета
+        {
+            colorDialog1.ShowDialog();
+            brush = new Pen(colorDialog1.Color, brush.Width);
+            pbColor.BackColor = colorDialog1.Color;
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) // ращмер 
+        {
+            brush = new Pen(brush.Color, Convert.ToInt32(numericUpDown1.Value));
+        }
+
+        // управление мышью
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            X1 = e.X;
+            Y1 = e.Y;
+            isPressed = true;
+        }
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            switch(tool)
+            {
+                case 1:
+                    if (isPressed)
+                    {
+                        myLine(X1, Y1, e.X, e.Y);
+                        X1 = e.X;
+                        Y1 = e.Y;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            X2 = e.X;
+            Y2 = e.Y;
+            switch (tool)
+            {
+                case 2:
+                    myLine(X1, Y1, X2, Y2);
+                    break;
+                case 3:
+                    myTriangle(X1, Y1, X2, Y2);
+                    break;
+                case 4:
+                    myRectangle(X1, Y1, X2, Y2);
+                    break;
+                case 5:
+                    myСircle(X1, Y1, X2, Y2);
+                    break;
+                case 6:
+                    myEllipse(X1, Y1, Math.Abs(e.X - X1), Math.Abs(e.Y - Y1));
+                    break;
+                default:
+                    break;
+            }
+            isPressed = false;
+        }
+        
+        // инструменты
+        private void button1_Click(object sender, EventArgs e) // очистить
+        {
+            G.Clear(Color.White);
+        }
+        private void btnPencil_Click(object sender, EventArgs e) // карандаш
+        {
+            tool = 1;
+        }
+        private void btnLine_Click(object sender, EventArgs e) // прямая
+        {
+            tool = 2;
+        }
+        private void btnTriangle_Click(object sender, EventArgs e) // треугольник
+        {
+            tool = 3;
+        }
+        private void btnRectangle_Click(object sender, EventArgs e) // прямоугольник
+        {
+            tool = 4;
+        }
+        private void btnCircle_Click(object sender, EventArgs e) // окружность
+        {
+            tool = 5;
+        }
+        private void btnEllipse_Click(object sender, EventArgs e) // эллипс
+        {
+            tool = 6;
+        }
+        private void btnFractal_Click(object sender, EventArgs e) // кнопка для рисования фрактала
+        {
+            myFractal(780, 374, G, brush);
+        }
+
+
+        // фигуры 
+        public void myPoint(float x, float y) // точка
+        {
+            G.DrawEllipse(brush, x, y, brush.Width, brush.Width);
+        }
+        public void myLine(float x1, float y1, float x2, float y2) // линия
+        {
+            float deltaX = Math.Abs(x2 - x1);
+            float deltaY = Math.Abs(y2 - y1);
+            float signX = x1 < x2 ? 1 : -1;
+            float signY = y1 < y2 ? 1 : -1;
+            
+            float error = deltaX - deltaY;
+            myPoint(x1, y1);
+            while (x1 != x2 || y1 != y2)
+            {
+                myPoint(x1, y1);
+                float error2 = error * 2;
+
+                if (error2 > -deltaY)
+                {
+                    error -= deltaY;
+                    x1 += signX;
+                }
+                if (error2 < deltaX)
+                {
+                    error += deltaX;
+                    y1 += signY;
+                }
+            }
+        }
+        public void myTriangle(float x1, float y1, float x2, float y2) // треугольник
+        {
+            myLine(X1, Y1, X2, Y2);
+            myLine(X1, Y1, X1 - (X2 - X1), Y2); 
+            myLine(X1 - (X2 - X1), Y2, X2, Y2);
+
+        }
+        public void myRectangle(float x1, float y1, float x2, float y2) // прямоугольник
+        {
+            myLine(X1, Y1, X1, Y2);
+            myLine(X1, Y1, X2, Y1);
+            myLine(X2, Y2, X1, Y2);
+            myLine(X2, Y2, X2, Y1);
+        }
+        public void myСircle(float x1, float y1, float x2, float y2) // окружность
+        {
+            double R = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+            for (int i = 0; i < 2880; i++)
+            {
+                myPoint(x1 + (float)(R * Math.Cos(2 * Math.PI / 2880 * i)), y1 + (float)(R * Math.Sin(2 * Math.PI / 2880 * i)));
+            }
+
+        }
+        public void myEllipse(float x, float y, float a, float b) // эллипс
+        {
+            int col, row;
+            long S_a, S_b, two_S_a, two_S_b, four_S_a, four_S_b, d;
+
+            S_b = (int)(b * b);
+            S_a = (int)(a * a);
+            row = (int)b;
+            col = 0;
+            two_S_a = S_a << 1;
+            four_S_a = S_a << 2;
+            four_S_b = S_b << 2;
+            two_S_b = S_b << 1;
+            d = two_S_a * ((row - 1) * (row)) + S_a + two_S_b * (1 - S_a);
+            while (S_a * (row) > S_b * (col))
+            {
+                myPoint(col + x, row + y);
+                myPoint(col + x, y - row);
+                myPoint(x - col, row + y);
+                myPoint(x - col, y - row);
+                if (d >= 0)
+                {
+                    row--;
+                    d -= four_S_a * (row);
+                }
+                d += two_S_b * (3 + (col << 1));
+                col++;
+            }
+            d = two_S_b * (col + 1) * col + two_S_a * (row * (row - 2) + 1) + (1 - two_S_a) * S_b;
+            while (row + 1 > 0)
+            {
+                myPoint(col + x, row + y);
+                myPoint(col + x, y - row);
+                myPoint(x - col, row + y);
+                myPoint(x - col, y - row);
+                if (d <= 0)
+                {
+                    col++;
+                    d += four_S_b * col;
+                }
+                row--;
+                d += two_S_a * (3 - (row << 1));
+            }
+        }
+
+        // фрактал
+        public void myFractal(int w, int h, Graphics g, Pen pen) // код фрактала
+        {
+            double cRe, cIm;
+            double newRe, newIm, oldRe, oldIm;
+            double zoom = (double)nudZoom.Value;
+            double moveX = (double)nudX.Value;
+            double moveY = (double)nudY.Value;
+            int maxIterations = 300;
+
+            // это определяет форму фрактала 
+            cRe = -0.70176;
+            cIm = -0.3842;
+            
+            for (int x = 0; x < w; x++)
+                for (int y = 0; y < h; y++)
+                {
+                    newRe = 1.5 * (x - w / 2) / (0.5 * zoom * w) + moveX;
+                    newIm = (y - h / 2) / (0.5 * zoom * h) + moveY;
+                    
+                    int i;
+                    
+                    for (i = 0; i < maxIterations; i++)
+                    {
+                        oldRe = newRe;
+                        oldIm = newIm;
+                        
+                        newRe = oldRe * oldRe - oldIm * oldIm + cRe;
+                        newIm = 2 * oldRe * oldIm + cIm;
+
+                        if ((newRe * newRe + newIm * newIm) > 4) break;
+                    }
+
+                    pen.Color = Color.FromArgb(255, (i * 9) % 255, 0, (i * 9) % 255);
+                    myPoint(x, y);
+                }
+        }
+    }
+}
